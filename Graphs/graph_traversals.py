3b5e84540e25536bef89a8e7f079aa18f08a97d9
Ben Gummoe <b.gummoe@gmail.com>
2018-10-21 16:22:00 -0400

    Implemented BFS

--- Graphs/graph_traversals.py
+++ Graphs/graph_traversals.py
@@ -1,7 +1,22 @@
+bfs_queue = []
 
-def depth_first_search(graph):
+
+def depth_first_search(graph, val):
     pass
 
 
-def breadth_first_search(graph):
-    pass
+def breadth_first_search(graph, val):
+    return bfs_node_search(graph.nodes[0], val)
+
+
+def bfs_node_search(node, val):
+    if node.data == val:
+        return True
+
+    for node in node.nodes:
+        bfs_queue.append(node)
+
+    if len(bfs_queue) > 1:
+        return bfs_node_search(bfs_queue.pop(0), val)
+    else:
+        return False
--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -2,25 +2,43 @@ import unittest
 
 from Graphs.graph import Graph
 from Graphs.graph_node import GraphNode
+from Graphs.graph_traversals import breadth_first_search
 
 
-def construct_circular_graph():
+def construct_undirected_graph():
     """
      1 -> 2
      ^    |
      |    v
-        3
+      --- 3 ---> 4
+          |
+          V
+          5
 
     """
     graph = Graph()
     graph_node_1 = GraphNode(1)
     graph_node_2 = GraphNode(2)
     graph_node_3 = GraphNode(3)
+    graph_node_4 = GraphNode(4)
+    graph_node_5 = GraphNode(5)
     graph_node_1.add_node(graph_node_2)
     graph_node_2.add_node(graph_node_3)
     graph_node_3.add_node(graph_node_1)
+    graph_node_3.add_node(graph_node_4)
+    graph_node_4.add_node(graph_node_5)
     graph.add_nodes([graph_node_1, graph_node_2, graph_node_3])
+    return graph
 
 
 class GraphsTest(unittest.TestCase):
-    pass
+
+    def test_bfs_search_value_in_graph(self):
+        graph = construct_undirected_graph()
+
+        self.assertTrue(breadth_first_search(graph, 2))
+
+    def test_bfs_search_value_not_in_graph(self):
+        graph = construct_undirected_graph()
+
+        self.assertFalse(breadth_first_search(graph, 18))
