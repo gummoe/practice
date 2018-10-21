947192c11c4fd49e6ace4e22b3c761a22daae6c5
Ben Gummoe <b.gummoe@gmail.com>
2018-10-21 10:34:34 -0400

    Fix Tree traversal implementation - copy paste error!

--- Trees/tree_traversals.py
+++ Trees/tree_traversals.py
@@ -1,31 +1,33 @@
-collection = []
+in_order_collection = []
+pre_order_collection = []
+post_order_collection = []
 
 
 def in_order_traversal(node):
     if node is not None:
         in_order_traversal(node.left)
-        collection.append(node.data)
+        in_order_collection.append(node.data)
         in_order_traversal(node.right)
 
-    return collection
+    return in_order_collection
 
 
 def pre_order_traversal(node):
     if node is not None:
-        collection.append(node.data)
-        in_order_traversal(node.left)
-        in_order_traversal(node.right)
+        pre_order_collection.append(node.data)
+        pre_order_traversal(node.left)
+        pre_order_traversal(node.right)
 
-    return collection
+    return pre_order_collection
 
 
 def post_order_traversal(node):
     if node is not None:
-        in_order_traversal(node.left)
-        in_order_traversal(node.right)
-        collection.append(node.data)
+        post_order_traversal(node.left)
+        post_order_traversal(node.right)
+        post_order_collection.append(node.data)
 
-    return collection
+    return post_order_collection
 
 
 
